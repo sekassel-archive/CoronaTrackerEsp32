a01ebd5948aae6e35da926661f8e6be2d8002576
Yannick Nagel <ynnck@gmx.de>
2020-06-15 11:30:39 +0200

    Added initial Infectionmap

--- server/src/main/java/de/uniks/Main.java
+++ server/src/main/java/de/uniks/Main.java
@@ -1,14 +1,30 @@
 package de.uniks;
 
-import static spark.Spark.get;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static spark.Spark.*;
 
 public class Main {
 
+    private static HashMap<Long, ArrayList<Integer>> infections = new HashMap<>();
+
     public static void main(String[] args) {
         get("/hello", (request, response) -> {
-            System.out.print(request.userAgent());
-            System.out.println(" said Hello!");
             return "Hello World";
         });
+
+        get("/infections", ((request, response) -> {
+            return infections;
+        }));
+
+        post("/infections", ((request, response) -> {
+            long time = System.currentTimeMillis();
+            if (!infections.containsKey(time)) {
+                infections.put(time, new ArrayList<>());
+            }
+            infections.get(time).add(Integer.parseInt(request.body()));
+            return "Success!";
+        }));
     }
 }
