d662b5ea7441fa829e0721d76a2a728059cc02b7
Yannick Nagel <ynnck@gmx.de>
2020-06-15 12:45:56 +0200

    Posted JSON is now parsed

---
+++ server/src/main/java/de/uniks/InfectionPostPayload.java
@@ -0,0 +1,13 @@
+package de.uniks;
+
+import lombok.Data;
+
+@Data
+public class InfectionPostPayload {
+    private long time;
+    private int id;
+
+    public boolean isValid() {
+        return time > 0 && (int) (Math.log10(time)+1) == 10 && id > 0;
+    }
+}
--- server/src/main/java/de/uniks/Main.java
+++ server/src/main/java/de/uniks/Main.java
@@ -1,29 +1,46 @@
 package de.uniks;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 
-import static spark.Spark.*;
+import static java.net.HttpURLConnection.HTTP_BAD_REQUEST;
+import static spark.Spark.get;
+import static spark.Spark.post;
 
 public class Main {
 
     private static HashMap<Long, ArrayList<Integer>> infections = new HashMap<>();
 
     public static void main(String[] args) {
-        get("/hello", (request, response) -> {
-            return "Hello World";
-        });
+        get("/hello", (request, response) -> "Hello World");
 
         get("/infections", ((request, response) -> {
-            return infections;
+            return new JSONObject(infections);
         }));
 
         post("/infections", ((request, response) -> {
-            long time = System.currentTimeMillis();
-            if (!infections.containsKey(time)) {
-                infections.put(time, new ArrayList<>());
+            ObjectMapper mapper = new ObjectMapper();
+            InfectionPostPayload input;
+            try {
+                input = mapper.readValue(request.body(), InfectionPostPayload.class);
+            } catch (JsonParseException e) {
+                response.status(HTTP_BAD_REQUEST);
+                return e.getMessage();
             }
-            infections.get(time).add(Integer.parseInt(request.body()));
+
+            if(!input.isValid()) {
+                response.status(HTTP_BAD_REQUEST);
+                return "Request values invalid";
+            }
+
+            if(!infections.containsKey(input.getTime())) {
+                infections.put(input.getTime(), new ArrayList<>());
+            }
+            infections.get(input.getTime()).add(input.getId());
             return "Success!";
         }));
     }
