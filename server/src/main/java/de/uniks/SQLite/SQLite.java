2bf6740e561fa204929ce0a9e35354cbd4347f88
Yannick Nagel <ynnck@gmx.de>
2020-09-14 11:15:34 +0200

    Added try-with-resources blocks to properly close database connections

--- server/src/main/java/de/uniks/SQLite/SQLite.java
+++ server/src/main/java/de/uniks/SQLite/SQLite.java
@@ -14,8 +14,6 @@ public class SQLite {
 
     private static final String DATABASE_PATH = "jdbc:sqlite:ExposureDatabase.db";
 
-    private static final String CREATE_RSIN_SQL = "CREATE TABLE IF NOT EXISTS RSIN (rsin integer UNIQUE);";
-
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
         try {
@@ -29,53 +27,54 @@ public class SQLite {
     }
 
     public static void initializeDatabase() throws SQLException {
-        Connection conn = DriverManager.getConnection(DATABASE_PATH);
+        final String CREATE_RSIN_SQL = "CREATE TABLE IF NOT EXISTS RSIN (rsin integer UNIQUE);";
 
-        Statement stmt = conn.createStatement();
-        stmt.execute(CREATE_RSIN_SQL);
-
-        conn.close();
+        try (Connection conn = DriverManager.getConnection(DATABASE_PATH); Statement stmt = conn.createStatement()) {
+            stmt.execute(CREATE_RSIN_SQL);
+        }
     }
 
     public static void insertExposures(Map<Integer, List<byte[]>> exposures) throws SQLException {
         final String INSERT_SQL = "INSERT INTO RSIN_?? VALUES(?);";
 
-        Connection conn = DriverManager.getConnection(DATABASE_PATH);
-        createRSINTables(exposures.keySet(), conn);
+        try (Connection conn = DriverManager.getConnection(DATABASE_PATH)) {
+            createRSINTables(exposures.keySet(), conn);
 
-        for (Map.Entry<Integer, List<byte[]>> entry : exposures.entrySet()) {
-            String sql = INSERT_SQL.replace("??", entry.getKey().toString());
-            PreparedStatement pstmt = conn.prepareStatement(sql);
-            for (byte[] key_data : entry.getValue()) {
-                pstmt.setBytes(1, key_data);
-                try {
-                    pstmt.executeUpdate();
-                } catch (SQLiteException e) {
-                    if (e.getErrorCode() != Codes.SQLITE_CONSTRAINT) throw e;
+            for (Map.Entry<Integer, List<byte[]>> entry : exposures.entrySet()) {
+                String sql = INSERT_SQL.replace("??", entry.getKey().toString());
+                try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+                    for (byte[] key_data : entry.getValue()) {
+                        pstmt.setBytes(1, key_data);
+                        try {
+                            pstmt.executeUpdate();
+                        } catch (SQLiteException e) {
+                            if (e.getErrorCode() != Codes.SQLITE_CONSTRAINT) {
+                                throw e; //SQLITE_CONSTRAINT simply means that the value already exists
+                            }
+                        }
+                    }
                 }
             }
         }
-
-        conn.close();
     }
 
     private static void createRSINTables(Set<Integer> rsins, Connection db) throws SQLException {
-        final String CREATE_SQL = "CREATE TABLE IF NOT EXISTS RSIN_? (key_data BLOB UNIQUE);";
+        final String CREATE_SQL = "CREATE TABLE IF NOT EXISTS RSIN_?? (key_data BLOB UNIQUE);";
         final String INSERT_SQL = "INSERT INTO RSIN VALUES (?)";
 
-        Statement stmt = db.createStatement();
-        PreparedStatement pstmt = db.prepareStatement(INSERT_SQL);
-
-        for (Integer rsin : rsins) {
-            String sql = CREATE_SQL.replace("?", rsin.toString());
-            stmt.execute(sql);
-            pstmt.setInt(1, rsin);
-            try {
-                pstmt.executeUpdate();
-            } catch (SQLiteException e) {
-                if (e.getErrorCode() != Codes.SQLITE_CONSTRAINT)
-                    throw e; //SQLITE_CONSTRAINT simply means that the value already exists
+        try (Statement stmt = db.createStatement(); PreparedStatement pstmt = db.prepareStatement(INSERT_SQL)) {
+            for (Integer rsin : rsins) {
+                stmt.addBatch(CREATE_SQL.replace("??", rsin.toString()));
+                pstmt.setInt(1, rsin);
+                try {
+                    pstmt.executeUpdate();
+                } catch (SQLiteException e) {
+                    if (e.getErrorCode() != Codes.SQLITE_CONSTRAINT) {
+                        throw e; //SQLITE_CONSTRAINT simply means that the value already exists
+                    }
+                }
             }
+            stmt.executeBatch();
         }
     }
 
@@ -83,49 +82,59 @@ public class SQLite {
         final String SELECT_SQL = "SELECT rsin FROM RSIN";
         Map<Integer, Integer> sizesMap = new HashMap<>();
 
-        Connection conn = DriverManager.getConnection(DATABASE_PATH);
-        ResultSet resultSet = conn.createStatement().executeQuery(SELECT_SQL);
-
-        while (resultSet.next()) {
-            int rsin = resultSet.getInt("rsin");
-            int size = getTableSize(Integer.toString(rsin), conn);
-            sizesMap.put(rsin, size);
+        try (Connection conn = DriverManager.getConnection(DATABASE_PATH);
+             Statement stmt = conn.createStatement();
+             ResultSet resultSet = stmt.executeQuery(SELECT_SQL)) {
+            while (resultSet.next()) {
+                int rsin = resultSet.getInt("rsin");
+                int size = getTableSize(Integer.toString(rsin), conn);
+                sizesMap.put(rsin, size);
+            }
+            return sizesMap;
         }
-        return sizesMap;
     }
 
     //Deletes databases older than 2 weeks
     public static void cleanUpDatabases() throws SQLException {
         final String SELECT_SQL = "SELECT rsin FROM RSIN WHERE rsin < ?";
         int cutoff = CWACryptography.getRollingStartIntervalNumber(System.currentTimeMillis() / 1000)
-                - (CWACryptography.TWO_WEEKS_IN_10_MINUTES_INTERVAL * 2 + 144); //CWA keeps rsins for a month it seems
+                - (CWACryptography.TWO_WEEKS_IN_10_MINUTES_INTERVAL + 144); //CWA keeps rsins for a month it seems
 
-        Connection conn = DriverManager.getConnection(DATABASE_PATH);
 
-        PreparedStatement stmt = conn.prepareStatement(SELECT_SQL);
-        stmt.setInt(1, cutoff);
-        ResultSet resultSet = stmt.executeQuery();
+        try (Connection conn = DriverManager.getConnection(DATABASE_PATH)) {
+            ArrayList<Integer> rsinList = new ArrayList<>();
 
-        ArrayList<Integer> rsinList = new ArrayList<>();
-        while (resultSet.next()) {
-            rsinList.add(resultSet.getInt("rsin"));
+            try (PreparedStatement pstmt = conn.prepareStatement(SELECT_SQL)) {
+                pstmt.setInt(1, cutoff);
+                try (ResultSet resultSet = pstmt.executeQuery()) {
+                    while (resultSet.next()) {
+                        rsinList.add(resultSet.getInt("rsin"));
+                    }
+                }
+            }
+
+            final String DELETE_SQL = "DELETE FROM RSIN WHERE rsin < ?";
+
+            try (PreparedStatement pstmt = conn.prepareStatement(DELETE_SQL)) {
+                pstmt.setInt(1, cutoff);
+                pstmt.execute();
+            }
+            try (Statement statement = conn.createStatement()) {
+                for (Integer rsin : rsinList) {
+                    final String DROP_SQL = "DROP TABLE RSIN_" + rsin;
+                    statement.addBatch(DROP_SQL);
+                }
+                statement.executeBatch();
+            }
         }
-        final String DELETE_SQL = "DELETE FROM RSIN WHERE rsin < ?";
-        stmt = conn.prepareStatement(DELETE_SQL);
-        stmt.setInt(1, cutoff);
-        stmt.execute();
-
-        Statement statement = conn.createStatement();
-        for (Integer rsin : rsinList) {
-            final String DROP_SQL = "DROP TABLE RSIN_" + rsin;
-            statement.addBatch(DROP_SQL);
-        }
-        statement.executeBatch();
     }
 
     private static int getTableSize(String name, Connection db) throws SQLException {
         final String SELECT_SQL = "SELECT COUNT(*) FROM RSIN_" + name;
-        return db.createStatement().executeQuery(SELECT_SQL).getInt(1);
+
+        try (Statement stmt = db.createStatement(); ResultSet resultSet = stmt.executeQuery(SELECT_SQL)) {
+            return resultSet.getInt(1);
+        }
     }
 
     //For Debugging purposes
@@ -133,45 +142,47 @@ public class SQLite {
     public static void addTemporaryExposureKey(int rsin, byte[] tek) throws SQLException {
         final String SELECT_SQL = "INSERT INTO RSIN_?? VALUES (?)".replace("??", Integer.toString(rsin));
 
-        Connection conn = DriverManager.getConnection(DATABASE_PATH);
-
-        HashSet<Integer> rsinSet = new HashSet<>();
-        rsinSet.add(rsin);
-        createRSINTables(rsinSet, conn);
-
-        PreparedStatement stmt = conn.prepareStatement(SELECT_SQL);
-        stmt.setBytes(1, tek);
-        stmt.execute();
+        try (Connection conn = DriverManager.getConnection(DATABASE_PATH)) {
+            HashSet<Integer> rsinSet = new HashSet<>();
+            rsinSet.add(rsin);
+            createRSINTables(rsinSet, conn);
+            try (PreparedStatement pstmt = conn.prepareStatement(SELECT_SQL)) {
+                pstmt.setBytes(1, tek);
+                pstmt.execute();
+            }
+        }
     }
 
     public static List<byte[]> getRSINTable(int rsin) throws SQLException {
         final String SELECT_SQL = "SELECT * FROM RSIN_??".replace("??", Integer.toString(rsin));
 
-        Connection conn = DriverManager.getConnection(DATABASE_PATH);
-        ResultSet resultSet = conn.createStatement().executeQuery(SELECT_SQL);
+        try (Connection conn = DriverManager.getConnection(DATABASE_PATH);
+             Statement stmt = conn.createStatement();
+             ResultSet resultSet = stmt.executeQuery(SELECT_SQL)) {
+            ArrayList<byte[]> table = new ArrayList<>();
+            while (resultSet.next()) {
+                table.add(resultSet.getBytes(1));
+            }
 
-        ArrayList<byte[]> table = new ArrayList<>();
-        while (resultSet.next()) {
-            table.add(resultSet.getBytes(1));
+            return table;
         }
-
-        return table;
     }
 
     public static void deleteRollingStartIntervalNumber(int rsin) throws SQLException {
         final String DROP_SQL = "DROP TABLE RSIN_??".replace("??", Integer.toString(rsin));
 
-        Connection conn = DriverManager.getConnection(DATABASE_PATH);
-        conn.createStatement().execute(DROP_SQL);
+        try (Connection conn = DriverManager.getConnection(DATABASE_PATH); Statement stmt = conn.createStatement()) {
+            stmt.execute(DROP_SQL);
+        }
     }
 
     public static void deleteTemporaryExposureKey(int rsin, byte[] tek) throws SQLException {
         final String DELETE_SQL = "DELETE FROM RSIN_?? WHERE key_data = ?".replace("??", Integer.toString(rsin));
 
-        Connection conn = DriverManager.getConnection(DATABASE_PATH);
-        PreparedStatement stmt = conn.prepareStatement(DELETE_SQL);
-        stmt.setBytes(1, tek);
-        stmt.execute();
+        try (Connection conn = DriverManager.getConnection(DATABASE_PATH); PreparedStatement stmt = conn.prepareStatement(DELETE_SQL)) {
+            stmt.setBytes(1, tek);
+            stmt.execute();
+        }
     }
 
     //For Websocket
@@ -182,7 +193,10 @@ public class SQLite {
 
     public static byte[] getKeyData(int rsin, int index, Connection db) throws SQLException {
         final String SELECT_SQL = "SELECT key_data FROM RSIN_" + rsin + " WHERE rowid=" + (index + 1);//Database is not zero indexed
-        return db.createStatement().executeQuery(SELECT_SQL).getBytes(1);
+
+        try (Statement stmt = db.createStatement(); ResultSet resultSet = stmt.executeQuery(SELECT_SQL)) {
+            return resultSet.getBytes(1);
+        }
     }
 
     public static void closeDatabase(Connection db) throws SQLException {
