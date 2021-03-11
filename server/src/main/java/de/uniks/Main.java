1433b9e9a2e6f7d325fca0f3c22f7134ba1c55f4
maxtrinks <DTrinks@gmx.de>
2021-03-11 14:27:30 +0100

    uuid for testing

--- server/src/main/java/de/uniks/Main.java
+++ server/src/main/java/de/uniks/Main.java
@@ -21,6 +21,7 @@ import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -37,6 +38,8 @@ public class Main {
     public static void main(String[] args) {
 
         get("/api/hello", (request, response) -> "Hello World");
+
+        get("/api/uuid", (request, response) -> UUID.randomUUID().toString());
 
         get("/api/cwa/status", (request, response) -> cwaStatus);
 
