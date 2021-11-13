d7ec2dd1783e543e48bc1af538af2e9aea300f71
drinkbuddy <44676932+drinkbuddy@users.noreply.github.com>
2021-11-13 23:35:43 +0100

    Update sw.js

--- flasher/sw.js
+++ flasher/sw.js
@@ -10,7 +10,7 @@ if ('serviceWorker' in navigator) {
   });
 }
 
-var CACHE_NAME = 'cache-v30';
+var CACHE_NAME = 'cache-v31';
 var urlsToCache = [
   '/css/serialisactive.css',
   '/css/serialnotactive.css',
