20a45654ff1e237eb71ea489225ac2195fbf7c47
Jonathan Meier <meier-jonathan@t-online.de>
2021-11-15 18:50:48 +0100

    add transformer test

---
+++ flasher/tests/SlipFrameTransformer.test.js
@@ -0,0 +1,9 @@
+import { test } from 'uvu';
+import * as assert from 'uvu/assert';
+import { SlipFrameTransformer } from '../js/SlipFrameTransformer.js';
+
+test('Test SlipFramTransformer', () => {
+    
+});
+
+test.run()
